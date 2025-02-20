51e138a67bfb9269890f9de1f7ad790216cc527e
AKSHAT KUMAR SINGH <136428913+AKSHATKUAMRSINGH@users.noreply.github.com>
2025-02-20 17:16:29 +0530

    Add files via upload

---
+++ letterrighttraingle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (char ch = 'A'; ch<= 'A'+i; ch++)
+        {
+            cout << ch << " ";
+            
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter number of rows and columns: ";
+    cin >> n;
+    pattern1(n);
+    return 0;
+}
