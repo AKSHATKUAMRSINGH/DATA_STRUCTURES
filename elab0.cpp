cac1d4388ae428d81602a5329627bf5e1b4a143e
AKSHAT KUMAR SINGH <136428913+AKSHATKUAMRSINGH@users.noreply.github.com>
2025-02-23 00:37:15 +0530

    Add files via upload

---
+++ elab0.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+ cout<<"return(l-2x)(b-2x)x;";
+}
+int main()
+{
+ int tc;
+ double a, b, c, res, l, w, x;
+ scanf(" %d", &tc);
+ while(tc--) {
+ scanf(" %lf %lf", &l, &w);
+ a = 12.0;
+ b = -4.0 * (l+w);
+ c = lw;
+ x = (-b - sqrt (bb - 4.0ac)) / (2.0a);
+ res = (l - 2x) * (w - 2x) x;
+ printf ("%.9f\n", res);
+ }
+ return 0;
+}
