a76ea8d38648f9d7591e606a0d533be10f7c371e
AKSHAT KUMAR SINGH <136428913+AKSHATKUAMRSINGH@users.noreply.github.com>
2025-03-09 19:34:50 +0530

    Add files via upload

---
+++ reverse_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int rev_num=0;
+    while(n>0){
+        int ld=n%10;
+        rev_num=(rev_num*10)+ld;
+        n=n/10;
+    }
+    cout<rev_num;
+    return 0;
+}
