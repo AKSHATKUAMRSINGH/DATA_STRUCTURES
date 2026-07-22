18cc4bd5c98d32f6b5263d866f140c92238959fe
AKSHAT KUMAR SINGH <136428913+AKSHATKUAMRSINGH@users.noreply.github.com>
2026-07-22 23:07:21 +0530

    Add files via upload

---
+++ second_largest_element_in_array.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+int findSecondLargest(int n, vector<int> &arr)
+{
+    // Write your code here.
+    if (n < 2)
+       return -1;
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] != largest && arr[i] > secondLargest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN)
+        return -1;
+
+    return secondLargest;
+}
